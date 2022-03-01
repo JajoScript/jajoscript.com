876cac6caccc1917110ca5bb0b95bf0c1c85e9aa
Javier Almarza <Javieralmarzabucarey@gmail.com>
2022-03-01 05:41:09 -0300

    tracking commits

--- src/components/Portfolio/Proyect.jsx
+++ src/components/Portfolio/Proyect.jsx
@@ -1,5 +1,6 @@
 // Dependencias.
 import React, { Component } from 'react';
+import { BiGitBranch } from 'react-icons/bi';
 
 // Estilos.
 // Otros componentes.
@@ -7,13 +8,52 @@ import React, { Component } from 'react';
 
 // Definición del componente.
 export default class Proyect extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  componentWillMount() {
+    this.getDataRepo();
+  }
+
+  getDataRepo = () => {
+    fetch(
+      `https://api.github.com/repos/JajoScript/${this.props.nameRepository}/contributors`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        let contributions = 0;
+
+        data.map((item) => {
+          contributions += item.contributions;
+        });
+
+        this.setState({
+          contributions: contributions,
+        });
+      })
+      .catch((e) => {
+        this.setState({
+          contributions: '0',
+        });
+      });
+  };
+
   render() {
     return (
       <article className="portfolio__item">
         <div className="portfolio__item-img">
           <img src={this.props.urlImg} alt={this.props.altImg} />
         </div>
+
         <h3>{this.props.title}</h3>
+
+        <div className="portfolio__contributions">
+          <BiGitBranch />
+          <h5 className="text-light">Aportes: {this.state.contributions}</h5>
+        </div>
 
         <div className="portfolio__item-btns">
           <a href={this.props.urlRepository} target="_blank" className="btn">
