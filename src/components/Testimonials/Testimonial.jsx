c6a1d8e4b694b2dbe09332451bbf51537451941e
Javier Almarza <Javieralmarzabucarey@gmail.com>
2022-03-01 07:26:16 -0300

    seccion de testimonios lista

---
+++ src/components/Testimonials/Testimonial.jsx
@@ -0,0 +1,65 @@
+// Dependencias.
+import { AiFillStar } from 'react-icons/ai';
+import React, { Component } from 'react';
+
+// Definición del componente.
+export default class Testimonial extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.getClienteData();
+  }
+
+  getClienteData = () => {
+    fetch('https://randomuser.me/api/')
+      .then((res) => res.json())
+      .then((client) => {
+        let data = client.results[0];
+        console.log(data);
+
+        let imgSrc = data.picture.large;
+        let phone = data.cell;
+        let name = `${data.name.first} ${data.name.last}`;
+
+        this.setState({
+          clientImg: imgSrc,
+          clientPhone: phone,
+          clientName: name,
+        });
+      })
+      .catch((e) => {
+        this.setState({
+          display: false,
+        });
+      });
+  };
+
+  render() {
+    return (
+      <article className="testimonial">
+        <div className="client__avatar">
+          <img src={this.state.clientImg} alt="Foto del cliente" />
+        </div>
+
+        <div className="client__info">
+          <h5 className="client__name">{this.state.clientName}</h5>
+          <small className="client__review">{this.props.review.desc}</small>
+
+          <div className="client__stars">
+            {[...Array(this.props.review.stars)].map((e, i) => (
+              <AiFillStar key={i} />
+            ))}
+          </div>
+        </div>
+      </article>
+    );
+  }
+}
+
+/* {new Array(this.props.stars).map((e, index) => {
+            return <AiFillStar key={`star-${index}`} />;
+          })} */
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,18 +1,73 @@
 // Dependencias.
+import { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import React, { Component } from 'react';
 
 // Estilos.
+import 'swiper/css';
+import 'swiper/css/pagination';
 import './Testimonials.css';
 
 // Otros componentes.
+import Testimonial from './Testimonial';
 
 // Definición del componente.
 export default class Testimonials extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  getReview = () => {
+    let reviews = [
+      {
+        desc: 'Le pedí una página hace 4 meses, me entrego un completo sin mayo. Increíble servicio.',
+        stars: 5,
+      },
+      {
+        desc: 'La página nunca funciono, pero buen cabro.',
+        stars: 4,
+      },
+      {
+        desc: 'Le pedí una página, quedo como el culo.',
+        stars: 3,
+      },
+      {
+        desc: 'Le pagué 2 palos, sigo esperando la página.',
+        stars: 2,
+      },
+      {
+        desc: 'Espere 2 años. Me hizo una página con WIX.',
+        stars: 1,
+      },
+    ];
+
+    let review = reviews[Math.floor(Math.random() * reviews.length)];
+
+    return review;
+  };
+
   render() {
     return (
       <section id="testimonials">
-        <h5>Reseñas de clientes</h5>
-        <h2>Próximamente ...</h2>
+        <h5>Reseñas de los clientes</h5>
+        <h2>Testimonios</h2>
+
+        <Swiper
+          className="container testimonials__container"
+          pagination={true}
+          spaceBetween={40}
+          modules={[Pagination]}
+        >
+          <SwiperSlide>
+            <Testimonial review={this.getReview()} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Testimonial review={this.getReview()} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Testimonial review={this.getReview()} />
+          </SwiperSlide>
+        </Swiper>
       </section>
     );
   }
